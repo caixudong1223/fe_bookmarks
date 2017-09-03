0fa6854caa44d9e4777d378ab5fd3f00aba63765
caixudong1223 <cxdazxm@163.com>
2017-09-03 19:41:33 +0800

    第2次提交

---
+++ src/components/VueContent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="vue-content">
+        <Row>
+            <Col span="6">
+                <Menu :theme="theme3" active-name="1">
+                    <MenuGroup title="内容管理">
+                        <MenuItem name="1">
+                            <Icon type="document-text"></Icon>
+                            文章管理
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="chatbubbles"></Icon>
+                            评论管理
+                        </MenuItem>
+                    </MenuGroup>
+                    <MenuGroup title="统计分析">
+                        <MenuItem name="3">
+                            <Icon type="heart"></Icon>
+                            用户留存
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <Icon type="heart-broken"></Icon>
+                            流失用户
+                        </MenuItem>
+                    </MenuGroup>
+                </Menu>
+            </Col>
+
+
+        </Row>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+
+</script>
+
+<style scoped type="less">
+    @import url('../assets/less/vuecontent.less');
+</style>
