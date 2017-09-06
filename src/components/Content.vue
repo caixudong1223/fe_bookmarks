71694c006ba7db58ebfb4ed2975d974beadbf9ef
caixudong1223 <cxdazxm@163.com>
2017-09-06 09:23:35 +0800

    key

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card-list">
         <Row :gutter="10">
-            <Col span="6" v-for="item in data">
+            <Col span="6" v-for="item in data" :key="item.index">
                 <a :href="item.siteurl">
                     <div class="card">
                         <Card>
@@ -9,8 +9,8 @@
                                 <img :src="item.picurl">
                             </div>
                             <h3>{{item.name}}</h3>
-                            <div class="card-tags">
-                                <Tag v-if="item.tags && item.tags.length > 0" v-for="(tag,index) in item.tags" :color="colors[index % 4]">{{tag}}</Tag>
+                            <div class="card-tags" v-if="item.tags && item.tags.length > 0">
+                                <Tag v-for="(tag,index) in item.tags" :color="colors[index % 4]" :key="index">{{tag}}</Tag>
                             </div>
                         </Card>
                     </div>
